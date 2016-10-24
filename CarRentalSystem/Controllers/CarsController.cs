76a4dbfec7528da2d61c6edb676b237e7a96d6cb
BakharEugene <eugenebakhar@gmail.com>
2016-10-25 01:11:06 +0300

    Project created

---
+++ CarRentalSystem/Controllers/CarsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarRentalSystem.DAL.EF;
+using CarRentalSystem.DAL.Models.Car;
+using CarRentalSystem.DAL.Models;
+
+namespace CarRentalSystem.Controllers
+{
+    public class CarsController : Controller
+    {
+        private CarRentalSystemContext db = new CarRentalSystemContext();
+
+        UnitOfWork unit = new UnitOfWork();
+        // GET: Cars
+        public ActionResult Index()
+        {
+            List<Car> Cars = db.Cars.ToList();
+            return View(Cars);
+        }
+
+        // GET: Cars/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Car car = db.Cars.Find(id);
+            if (car == null)
+            {
+                return HttpNotFound();
+            }
+            return View(car);
+        }
+
+        // GET: Cars/Create
+        public ActionResult Create()
+        {
+
+            SelectList Marks = new SelectList(db.Marks, "Id", "MarkType");
+            ViewBag.Marks = Marks;
+            return View();
+        }
+
+        // POST: Cars/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        public ActionResult Create(Car car)
+        {
+            car.Mark = unit.Marks.GetById(car.IdMark);
+            //Добавляем игрока в таблицу
+            db.Cars.Add(car);
+            
+            db.SaveChanges();
+            // перенаправляем на главную страницу
+            return RedirectToAction("Index");
+        }
+
+        // GET: Cars/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Car car = db.Cars.Find(id);
+            if (car == null)
+            {
+                return HttpNotFound();
+            }
+            return View(car);
+        }
+
+        // POST: Cars/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Model,Volume,Price,Description,IdMark")] Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(car).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(car);
+        }
+
+        // GET: Cars/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Car car = db.Cars.Find(id);
+            if (car == null)
+            {
+                return HttpNotFound();
+            }
+            return View(car);
+        }
+
+        // POST: Cars/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Car car = db.Cars.Find(id);
+            var g = "";
+            foreach (var pict in car.Pictures)
+            {
+                Picture picture = db.Pictures.Find(pict.Id);
+                db.Pictures.Remove(picture);
+
+                car.Pictures.Remove(pict);
+            }
+            db.Cars.Remove(car);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CarRentalSystem/Scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="jquery-3.1.1.js" />
+/// <reference path="jquery-3.1.1.min.js" />
+/// <reference path="jquery-3.1.1.intellisense.js" />
+/// <reference path="modernizr-2.6.2.js" />
+/// <reference path="respond.js" />
