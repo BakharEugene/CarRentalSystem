c1a53d4aac643d42d3bab5a89465635c68a126b1
BakharEugene <eugenebakhar@gmail.com>
2016-11-25 20:33:17 +0300

    views Cars vixed, added carousel on cars/details.

--- CarRentalSystem.DAL/Models/Users/RegisterModel.cs
+++ CarRentalSystem.DAL/Models/Users/RegisterModel.cs
@@ -10,26 +10,33 @@ namespace CarRentalSystem.DAL.Models.Users
 {
     public class RegisterModel
     {
-        [Required]
-        public string Email { get; set; }
-        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Input first name")]
+        [MaxLength(130, ErrorMessage = "Max length is 13")]
+        [MinLength(4, ErrorMessage = "Min length is 6")]
         public string FirstName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Input last name")]
+        [MaxLength(130, ErrorMessage = "Max length is 13")]
+        [MinLength(4, ErrorMessage = "Min length is 6")]
         public string LastName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Input telephone number")]
+        [MaxLength(13, ErrorMessage = "Max length is 13")]
+        [MinLength(6, ErrorMessage = "Min length is 6")]
+        public string Telephone { get; set; }
+        [Required(ErrorMessage = "Input your skype")]
+        public string Skype { get; set; }
+        public Enums.Genders Gender { get; set; }
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "Некорректный Email")]
+        [Required(ErrorMessage = "Input your email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage="Input password")]
+        [MinLength(5,ErrorMessage="Min length 5 symbols")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
-
-        [Required]
+        [Required(ErrorMessage = "Input password")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Пароли не совпадают")]
         public string ConfirmPassword { get; set; }
-        [Required]
-        public string Telephone { get; set; }
-        [Required]
-        public string Skype { get; set; }
-        [Required]
-        public Enums.Genders Gender { get; set; }
-
     }
 }
